7ab4d1be86781edae3c8654667117d46d7affdd5
unknown <datdonguyen99@gmail.com>
2021-11-25 16:52:28 +0700

    update-all

--- src/routes/scientist.routes.js
+++ src/routes/scientist.routes.js
@@ -2,7 +2,7 @@ const router = require("express-promise-router")();
 const scientistController = require("../controllers/scientist.controller");
 
 router.get("/scientist", scientistController.getListScientist);
-router.get("/scientist/:id", scientistController.getListScientistByID);
+router.get("/scientist/all/:id", scientistController.getListScientistByID);
 router.get("/scientist/reviewer", scientistController.getInforReviewer);
 router.get("/scientist/editor", scientistController.getInforEditor);
 router.get("/scientist/author", scientistController.getInforAuthor);
